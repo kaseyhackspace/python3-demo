47796e387919f0d214d5ddfc2e4705490bab3399
Kasey Martin <kasey.hackspace@gmail.com>
2019-03-13 12:07:22 +0000

    conditionals

--- main.py
+++ main.py
@@ -9,11 +9,20 @@ __license__ = "MIT"
 
 def main():
     """ Main entry point of the app """
-    print("hello world")
-    celsius = input('Input celsius here: ')
-    farenheit = (float(celsius)*1.8) + 32
-    print('celsius:'+celsius)
-    print('farenheit:'+str(farenheit))
+    print("1- Celsius to Fahrenheit\n2- Fahrenheit to Celsius\n")
+    choice = input('Choose a conversion: ')
+    if choice == '1':
+        celsius = input('Input celsius:')
+        fahrenheit = (float(celsius)*1.8) + 32
+        print('celsius:'+celsius)
+        print('fahrenheit:'+str(fahrenheit))
+    else:
+        fahrenheit = input('Input fahrenheit:')
+        celsius = (float(fahrenheit)-32) / 1.8
+        print('fahrenheit:'+fahrenheit)
+        print('celsius:'+str(celsius))
+        
+
 
 
 if __name__ == "__main__":
